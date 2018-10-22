1499f590d5bbe5bc27f92e61fcce51e61e4bfe95
neoflame99 <neoflame99@gmail.com>
2018-10-22 17:39:26 +0900

    adding

--- trait_exam/src/lib.rs
+++ trait_exam/src/lib.rs
@@ -1,6 +1,13 @@
 
 pub trait Summary {
-    fn summarize(&self) -> String;
+
+    fn summarize_author(&self) -> String;
+
+    //fn summarize(&self) -> String;
+    fn summarize(&self)-> String{
+        format!("(Read more from {}...)", self.summarize_author())
+    }
+
 }
 
 pub struct NewsArticle {
@@ -11,10 +18,15 @@ pub struct NewsArticle {
 }
 
 impl Summary for NewsArticle{
-    fn summarize(&self) -> String{
+/*    fn summarize(&self) -> String{
         format!("{}, by {} ({})", self.headline, self.author, self.location)
     }
+*/
+    fn summarize_author(&self) -> String{
+        format!("@{}", self.author)
+    }
 }
+
 
 pub struct Tweet {
     pub username: String,
@@ -22,8 +34,26 @@ pub struct Tweet {
     pub reply: bool,
     pub retweet: bool,
 }
+
 impl Summary for Tweet{
+    fn summarize_author(&self) -> String{
+        format!("@{}", self.username)
+    }
     fn summarize(&self)->String{
         format!("{}: {}", self.username, self.content)
     }
+
+}
+
+/*
+pub fn notify(item: impl Summary){
+    println!("Breaking news! {}", item.summarize());
+}
+pub fn notify<T: Summary>(item: T){
+    println!("Breaking news! {}", item.summarize());
+}
+*/
+pub fn notify<T: Summary>(item1: T, item2: T){
+    println!("Breaking news! {}", item1.summarize());
+    println!("Breaking news! {}", item2.summarize());
 }
--- trait_exam/src/main.rs
+++ trait_exam/src/main.rs
@@ -2,6 +2,7 @@
 
 extern crate trait_exam;
 use trait_exam::Tweet;
+use trait_exam::NewsArticle;
 use trait_exam::Summary;
 
 fn main() {
@@ -11,5 +12,49 @@ fn main() {
         reply: false,
         retweet: false,
     };
+    let tweet2 = Tweet {
+        username : String::from("pearson_ebooks"),
+        content: String::from("No matter"),
+        reply: false,
+        retweet: false,
+    };
     println!("1 new tweet: {}",tweet.summarize());
+
+    let article = NewsArticle{
+        headline: String::from("Penguins win the Stanley Cup Championship!"),
+        location: String::from("Pittsburgh, PA, USA"),
+        author: String::from("Iceburgh"),
+        content: String::from("The Pittsburgh Penguins once again are the best hockey team in the NHL"),
+    };
+    println!("News article available! {}", article.summarize());
+
+ //   trait_exam::notify(tweet); // using trait by argument
+    trait_exam::notify(tweet, tweet2); // using trait by argument
+
+    //-- trait bounds
+    let number_list = vec![34, 50 ,25 , 100, 65];
+    let result =  largest(&number_list);
+    println!("largest number is {}", result);
+}
+
+/*
+//-- trait bounds ways
+fn largest<T: PartialOrd + Copy >(list: &[T]) -> T{
+    let mut largest = list[0];
+    for &item in list.iter(){
+        if item > largest {
+            largest = item;
+        }
+    }
+    largest
+}*/
+//-- return reference ways
+fn largest<T: PartialOrd>(list: &[T]) -> &T{
+    let mut largest = &list[0];
+    for item in list.iter() {
+        if item > largest {
+            largest = &item;
+        }
+    }
+    &largest
 }
