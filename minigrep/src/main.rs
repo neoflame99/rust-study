7214f569e0c9e0dff4ecaa0e6e737c1c1658e8fd
neoflame99 <neoflame99@gmail.com>
2018-11-06 17:29:32 +0900

    minigrep changing

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,30 +1,28 @@
+extern crate minigrep;
 
 use std::env;
-use std::fs;
+use std::process;
+use minigrep::*;
 
 fn main() {
     let args : Vec<String>= env::args().collect();
+/*
+    let config = match Config::new(&args) {
+        Ok (Config) => Config,
+        Err(E)     => {println!("{}",E); std::process::exit(1) },
+    };
+*/
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
-    let narg = args.len();
-    if narg < 3 {
-        panic!("arguments are not enough");
-    }
-    let config = parse_config(&args);
-
-    println!("{}",narg);
     println!("Searching for {}",config.query);
     println!("in file {}",config.filename);
 
-    let contents = fs::read_to_string(config.filename).expect("Something went wrong reading the file");
-    println!("With text:\n{}",contents);
-}
-struct Config{
-    query : String,
-    filename : String,
-}
-fn parse_config(args: &[String]) -> Config{
-    Config {
-        query : args[1].clone(),
-        filename : args[2].clone(),
+    //run(config);
+    if let Err(e) = run(config) {
+        println!("Application error: {}", e);
+        process::exit(1);
     }
 }
