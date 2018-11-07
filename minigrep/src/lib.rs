82fa94b069de8e57b86439ae702fc28f799961f0
neoflame99 <neoflame99@gmail.com>
2018-11-07 17:18:48 +0900

    minigrep changing

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -22,6 +22,35 @@ impl Config {
 pub fn run (config: Config)->Result<(), Box<dyn Error>>{
     //let contents = fs::read_to_string(config.filename).expect("Something went wrong reading the file");
     let contents = fs::read_to_string(config.filename) ? ;
-    println!("With text:\n{}",contents);
+    let rslt = search( &config.query, &contents);
+    for item in rslt {
+        println!("{}", item);
+    }
+    //println!("With text:\n{}",contents);
     Ok(())
+}
+
+fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str>{
+    let mut List  = Vec::new();
+    for line in contents.lines(){
+        if line.contains(query) {
+            List.push( line);
+        }
+    }
+    List
+}
+
+#[cfg(test)]
+mod tests{
+    use super::*;
+
+    #[test]
+    fn one_result(){
+        let query = "duct";
+        let contents = "Rust:
+safe, fast, productive.
+Pick three.";
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+
+    }
 }
