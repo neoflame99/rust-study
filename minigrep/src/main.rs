dd9fbb4ea57a0d1d10d6ec057c28911a6d621876
neoflame99 <neoflame99@gmail.com>
2018-11-09 00:03:29 +0900

    error message to std err

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -13,7 +13,7 @@ fn main() {
     };
 */
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
+        eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
 
@@ -22,7 +22,7 @@ fn main() {
 
     //run(config);
     if let Err(e) = run(config) {
-        println!("Application error: {}", e);
+        eprintln!("Application error: {}", e);
         process::exit(1);
     }
 }
